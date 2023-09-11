9ab4e65ea6219a7a4f916e0aaefb41ed835cbc6d
QAEngineer2050 <144169921+QAEngineer2050@users.noreply.github.com>
2023-09-10 21:49:42 -0400

    Update SQL_query1.sql

--- SQL_query1.sql
+++ SQL_query1.sql
@@ -1,31 +1,31 @@
+--1 Retrieve all records from the actor table.
 select * from actor
-
+--2 Retrieve all records from the address table.
 select * from address
-
+--3  Retrieve all records from the category table.
 select * from category
-
+--4 Retrieve all records from the city table.
 select * from city
-
+--5 Retrieve all records from the country table.
 select * from country
-
+--6 Retrieve all records from the customer table.
 select * from customer
-
+--7 Retrieve all records from the film table.
 select * from film
-
+--8 Retrieve all records from the film_actor table.
 select * from film_actor
-
+--9 Retrieve all records from the film_category table
 select * from film_category
-
+--10 Retrieve all records from the inventory table.
 select * from inventory
-
+--11 Retrieve all records from the language table.
 select * from language
-
+--12 Retrieve all records from the payment table.
 select * from payment 
-
+--13 Retrieve all records from the rental table.
 select * from rental 
-
+--14 Retrieve all records from the staff table.
 select * from staff
-
+--16 Retrieve all records from the store table.
 select * from store 
-
 
